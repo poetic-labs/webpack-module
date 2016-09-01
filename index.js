99927a9a4738c7eeb4341c5491f0a76b3e13219f
martinbee <martin@poeticsystems.com>
2016-09-01 16:48:34 -0500

    set up the paths.:

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 // node node_modules/webpack/bin/webpack.js;
 
+var path = require("path");
 var webpack = require("webpack");
+
+var PATHS = {
+  src: path.join(__dirname, 'src'),
+  build: path.join(__dirname, 'build')
+};
 
 // returns a Compiler instance
 var compiler = webpack({
-  entry: "./index.js",
-    output: {
-        path: __dirname,
-        filename: "bundle.js"
+    entry: {
+      src: PATHS.src
     },
+    output: {
+      path: PATHS.build,
+      filename: 'bundle.js'
+    }
 });
 
 compiler.run(function(err, stats) {
-  console.log(stats || err);
+  if (err) {
+     console.log(err);
+  }
 });
