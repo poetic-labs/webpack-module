411e491daed7b9e62ddc845c7344955e3a87ef02
martinbee <martin@poeticsystems.com>
2016-09-02 09:41:30 -0500

    add webpack dir and config and read from those files to build.

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
 // node node_modules/webpack/bin/webpack.js;
 
+var fs = require("fs");
 var path = require("path");
 var webpack = require("webpack");
+var appRootDir = require('app-root-dir').get();
+
+// TODO write webpack config in webpack folder
+
+var webpackDir = appRootDir + '/webpack/';
+var webpackConfigFile = appRootDir + '/webpack/webpack.config.js';
+
+if (!fs.existsSync(webpackDir)) {
+  fs.mkdirSync(appRootDir + '/webpack/');
+}
+
+if (!fs.existsSync(webpackConfigFile)) {
+  var webpackConfig = (
+    `module.exports = {
+       entry: './index.js',
+       output: './bundle.js',
+     };`
+  );
+
+  fs.appendFileSync(appRootDir + '/webpack/webpack.config.js', webpackConfig);
+}
+
+// TODO read config when running webpack
+
+var config = require(webpackConfigFile);
+
+// TODO expose webpack and a webpack compile function
 
 function build() {
-
   // returns a Compiler instance
-  var compiler = webpack({
-      entry: {
-        src: './src/'
-      },
-      output: {
-        filename: 'bundle.js'
-      }
-  });
+  var compiler = webpack(config);
 
   compiler.run(function(err, stats) {
     if (err) {
