d5b5313e00f7a7e1ceddb7e148777acf2f721093
martinbee <martin@poeticsystems.com>
2016-09-06 13:52:08 -0500

    update to webpack 2 beta, update to use config.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var pkgConf = require('pkg-conf');
 var webpackModule = require('./src/webpackModule.js');
 
 // node node_modules/webpack/bin/webpack.js;
@@ -11,4 +12,18 @@ var webpackModule = require('./src/webpackModule.js');
 // TODO Build plugin that allows childrne to hook into events
 
 // console.log(webpackModule);
+
+pkgConf('dependencies').then(function(config) {
+    var namespaceRegex = /stanza-webpack*/;
+    var webpackModulePackages = Object.keys(config).filter(function(key) {
+      return key.match(namespaceRegex);
+    });
+
+    console.log(webpackModulePackages);
+
+    webpackModulePackages.forEach(function(package) {
+      console.log(package);
+    });
+});
+
 webpackModule.bundle();
--- src/webpackModule.js
+++ src/webpackModule.js
@@ -1,7 +1,6 @@
 var appRootDir = require('app-root-dir').get();
 var fs = require("fs");
 var webpackConfigFile = appRootDir + '/webpack/webpack.config.js';
-var pkgConf = require('pkg-conf');
 var webpack = require("webpack");
 var dynamicPlugin = require("./dynamicPlugin.js");
 
@@ -9,37 +8,15 @@ var dynamicPlugin = require("./dynamicPlugin.js");
 //
 // find from node modules then require and pass webpackModule
 
-pkgConf(/stanza-webpack*/).then(function(config) {
-    console.log(config);
-    //=> true
-});
-
 var webpackModule = {
   bundle: function() {
-    this.plugins.push({ plugin: dynamicPlugin })
-
-    var plugins =  this.plugins.map(function(pluginObject) {
-                     return new pluginObject.plugin();
-                   });
-
-    console.log(plugins);
-
-    var loaders = this.loaders;
-
-    var config = {
-      entry: './index.js',
-      output: './bundle.js',
-      plugins: plugins,
-      module: {
-        loaders: loaders,
-      }
-    };
+    this.register({ plugin: dynamicPlugin });
 
     if (fs.existsSync(webpackConfigFile)) {
       config = require(webpackConfigFile);
     }
 
-    var compiler = webpack(config);
+    var compiler = webpack(this.config);
 
     compiler.run(function(err, stats) {
       if (err) {
@@ -49,24 +26,29 @@ var webpackModule = {
     });
   },
 
-  loaders: [],
-
-  plugins: [],
+  config: {
+    entry: './index.js',
+    output: './bundle.js',
+    plugins: [],
+    module: {
+      loaders: [],
+    }
+  },
 
   register: function(child) {
     // child should be an object
 
-    if (typeof child !== object) {
+    if (typeof child !== 'object') {
       throw new Error('A registering child must be an object.');
     }
 
     if (child.loader || child.loaders) {
       // example loader child = { loader: 'babel', test: /\.js?$/ }
-      this.loaders.push(child);
+      this.config.module.loaders.push(child);
 
     } else if (child.plugin) {
-      // example plugin child = { plugin: dynamicPlugin, args: ['hello'] }
-      this.plugins.push(child);
+      // example plugin child = { plugin: dynamicPlugin }
+      this.config.plugins.push( new child.plugin() );
 
     } else {
       throw new Error('A registering child must declare a plugin, loader, or loaders property.');
