7d220cf312d2aba5a5a556152d93a9e608233902
martinbee <martin@poeticsystems.com>
2016-09-07 09:55:08 -0500

    add more compile events, refactor code.

--- src/compileEventPlugin.js
+++ src/compileEventPlugin.js
@@ -2,27 +2,38 @@ function compileEventPlugin(eventEmitter) {
   this.compileEventEmitter = eventEmitter;
 }
 
-// TODO: emit all events;
-
 compileEventPlugin.prototype.apply = function(compiler) {
-  compiler.plugin("compile", (params) => {
+  compiler.plugin('compile', (params) => {
     this.compileEventEmitter.emit('compile');
   });
 
-  compiler.plugin("compilation", (compilation) => {
+  compiler.plugin('compilation', (compilation) => {
     this.compileEventEmitter.emit('compilation');
 
-    compilation.plugin("optimize", () => {
+    compilation.plugin('optimize', () => {
       this.compileEventEmitter.emit('optimize');
     });
   });
 
-  compiler.plugin("emit", (compilation, callback) => {
+  compiler.plugin('emit', (compilation, callback) => {
     this.compileEventEmitter.emit('emit');
     callback();
   });
 
-  compiler.plugin("after-compile", (compilation, callback) => {
+  compiler.plugin('after-emit', (compilation, callback) => {
+    this.compileEventEmitter.emit('after-emit');
+    callback();
+  });
+
+  compiler.plugin('failed', (error) => {
+    this.compileEventEmitter.emit('failed');
+  });
+
+  compiler.plugin('invalid', (compilation) => {
+    this.compileEventEmitter.emit('invalid');
+  });
+
+  compiler.plugin('after-compile', (compilation, callback) => {
     this.compileEventEmitter.emit('after-compile');
     callback();
   });
