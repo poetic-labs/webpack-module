d7121f91ed61db4d486050c66565250c21d2a2e0
martinbee <martin@poeticsystems.com>
2016-09-08 15:10:19 -0500

    add search from kristy's code.

--- src/StanzaWebpack.js
+++ src/StanzaWebpack.js
@@ -1,9 +1,6 @@
 const appRootPath = require('app-root-dir').get();
-const findUp = require('find-up');
 const fs = require('fs');
-const path = require('path');
-const pkgConf = require('pkg-conf');
-const resolve = require('resolve');
+const setupDependencies = require('./setup-dependencies.js');
 
 const webpackConfigFile = appRootPath + '/webpack/webpack.config.js';
 const webpack = require('webpack');
@@ -12,33 +9,7 @@ class StanzaWebpack {
   constructor() {
     this.setDefaultConfig();
 
-    // TODO update dynamic require code with new code from Kristy
-
-    const devDependencies = pkgConf.sync('devDependencies');
-
-    Object.keys(devDependencies).forEach(dependency => {
-      try {
-        const dependencyPath = resolve.sync(dependency, { basedir: appRootPath });
-        const dependencyPackageJsonPath = findUp.sync('package.json', { cwd: dependencyPath });
-        const dependencyPackageJson = require(dependencyPackageJsonPath);
-
-        const keywords = dependencyPackageJson.keywords || [];
-
-        if (keywords.includes('stanza-webpack')) {
-          const extensionPath = path.dirname(dependencyPackageJsonPath);
-          const extension = require(extensionPath);
-
-          if (typeof extension.register === 'function') {
-            extension.register('stanza-webpack', this);
-          }
-        }
-      } catch (error) {
-        // TODO: Add better error handling
-        console.log(error);
-
-        return false;
-      }
-    });
+    setupDependencies('stanza-webpack', this.config);
 
     //TODO: Pass this.config to the extensible owner webpack config for final
     //say
@@ -49,6 +20,8 @@ class StanzaWebpack {
     // }
 
     this.compiler = webpack(this.config);
+
+    setupDependencies('stanza-webpack', this.compiler, 'requestCompiler');
   }
 
   //TODO: Create a config constructor that builds webpack client/server configs
---
+++ src/setup-dependencies.js
@@ -0,0 +1,65 @@
+const findUp = require('find-up');
+const path = require('path');
+const resolve = require('resolve');
+
+const structureDependencies = (dependencies, dependencyPath) => (
+  Object.keys(dependencies).map(dependency => (
+    {
+      basedir: dependencyPath,
+      name: dependency,
+    }
+  ))
+);
+
+const getCoreDependencies = () => {
+  const packageJsonPath = findUp.sync('package.json', { cwd: __dirname });
+  const coreDirectory = !packageJsonPath ? false : path.dirname(packageJsonPath);
+
+  const devDependencies = pkgConf.sync('devDependencies', { cwd: coreDirectory });
+  const dependencies = pkgConf.sync('dependencies', { cwd: coreDirectory });
+
+  const coreDependencies = devDependencies.concat(dependencies);
+
+  return structureDependencies(coreDependencies, coreDirectory);
+};
+
+const filterAndSetupDependencies = (dependencyPath, object, keyword, setupFunction) => {
+  const dependencyPackageJsonPath = findUp.sync('package.json', { cwd: dependencyPath });
+  const dependencyPackageJson = require(dependencyPackageJsonPath);
+  const keywords = dependencyPackageJson.keywords || [];
+
+  if (keywords.includes(keyword)) {
+    const extensionPath = path.dirname(dependencyPackageJsonPath);
+    const extension = require(extensionPath);
+
+    // why the dependencyPath?
+
+    extension[setupFunction](keyword, object, dependencyPath);
+  }
+};
+
+const setupDependencies = (keyword, object, setupFunction = 'register') => {
+  const devDependencies = pkgConf.sync('devDependencies');
+  const devDependenciesWithPath = structureDependencies(devDependencies, global.APP_ROOT_PATH);
+
+  const coreDependenciesWithPath = getCoreDependencies();
+
+  const allDependencies = [
+    ...coreDependenciesWithPath,
+    ...devDependenciesWithPath,
+  ];
+
+  allDependencies.forEach(dependency => {
+    const { basedir, name } = dependency;
+
+    try {
+      const dependencyPath = resolve.sync(name, { basedir });
+
+      filterAndSetupDependencies(dependencyPath, object, keyword, setupFunction);
+    } catch (error) {
+      console.log(`Error trying to resolve dependency ${name}. Continuing...`);
+    }
+  });
+};
+
+module.exports = setupDependencies;
